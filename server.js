ab601f52b23b7193f8d724f9801fb427ccc658d6
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2015-10-22 09:54:50 +1300

    Set up cookies/sessions

--- server.js
+++ server.js
@@ -5,12 +5,23 @@ var app = express()
 var morgan = require('morgan')
 var browserify = require('browserify')
 
+var cookieParser = require('cookie-parser')
+var session = require('express-session')
+
+
 app.use(morgan('dev'))
+app.use(cookieParser())
+app.use(session({secret: 'keyboard cat',
+                saveUninitialized: true,
+                resave: true}))
 
 app.get('/', function (req, res) {
   res.send('Hello World')
+  console.log(req.cookies)
+  console.log("=============")
+  console.log(req.session)
 })
 
 app.listen(3000)
 
-console.log("server running on port :" + 3000)
+console.log("server running on port " + 3000)
