19750c011ef1dd5dacdd30ca6276e7b92bf058d5
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2015-10-22 11:29:28 +1300

    Add folders app and config, also add database

---
+++ app/routes.js
@@ -0,0 +1,19 @@
+var User = require('./model/user')
+
+module.exports = function(app){
+  app.get('/', function(req, res) {
+    res.send('Hello World')
+  })
+
+  app.get('/:username/:password', function(req, res){
+    var newUser = new User()
+    newUser.local.username = req.params.username
+    newUser.local.password = req.params.password
+    console.log(newUser.username + " " + newUser.password)
+    newUser.save(function(err){
+      if (err)
+        throw err
+    })
+    res.send('SUCCESS')
+  })
+}
--- server.js
+++ server.js
@@ -2,25 +2,28 @@ var http = require('http');
 var express = require('express')
 var app = express()
 
-var morgan = require('morgan')
-var browserify = require('browserify')
-
 var cookieParser = require('cookie-parser')
 var session = require('express-session')
 
+var morgan = require('morgan')
+var browserify = require('browserify')
+var mongoose = require('mongoose')
+var configDB = require('./config/database.js')
+var bodyParser = require('body-parser')
+
+mongoose.connect(configDB.url)
 
 app.use(morgan('dev'))
 app.use(cookieParser())
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(session({secret: 'keyboard cat',
                 saveUninitialized: true,
                 resave: true}))
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-  console.log(req.cookies)
-  console.log("=============")
-  console.log(req.session)
-})
+app.set('view engine', 'ejs')
+
+require('./app/routes.js')(app)
+
 
 app.listen(3000)
 
