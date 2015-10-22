1332ab5013dc58021f0c10dbe8b0821cfdbb2377
Michaelwilsonm <1987wilsonm.MW@gmail.com>
2015-10-22 15:07:46 +1300

    Add signup.ejs and index.ejs

--- app/routes.js
+++ app/routes.js
@@ -2,7 +2,24 @@ var User = require('./model/user')
 
 module.exports = function(app){
   app.get('/', function(req, res) {
-    res.send('Hello World')
+    res.render('index.ejs')
+  })
+
+  app.get('/signup', function(req, res){
+    res.render('signup.ejs', {
+      message: 'Victory'
+    })
+  })
+
+  app.post('/signup', function(req, res){
+    var newUser = new User()
+    newUser.local.username = req.body.email
+    newUser.local.password = req.body.password
+    newUser.save(function(err){
+      if (err)
+        throw err
+    })
+    res.redirect('/')
   })
 
   app.get('/:username/:password', function(req, res){
